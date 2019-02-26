ef9dd3f43f028739c28e8282a22bbf529f462e66
Kevin Diem <kgd003@apca938.833chestnut.tju.edu>
2019-02-25 22:50:05 -0500

    add grid

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
-import { Grommet } from 'grommet';
+import { Box, Grid, Grommet } from 'grommet';
 
 class App extends Component {
   render() {
     return (
       <Grommet>
-        <div>
-
-        </div>
+        <Grid
+          areas={[
+            { name: 'list', start: [0, 0], end: [1, 0] },
+            { name: 'note', start: [1, 0], end: [1, 0] },
+          ]}
+          rows={['full']}
+          columns={['1/4', '3/4']}
+          gap="small"
+          justify="center"
+          style={{
+            height: '100vh'
+          }}
+        >
+          <Box gridArea="list" background="light-5">
+            {
+              // notes list
+            }
+          </Box>
+          <Box gridArea="note" xbackground="light-2">
+            {
+              // note area
+            }
+          </Box>
+        </Grid>
       </Grommet>
     );
   }
