b84e2563cb11cea7be569e0cd1f3ad91f3b60f1d
Kevin Diem <kgd003@apca938.833chestnut.tju.edu>
2019-02-25 23:59:50 -0500

    add reducer

---
+++ src/reducers/notes.js
@@ -0,0 +1,23 @@
+import * as actions from '../actions';
+
+export const initialState = {
+    notes: [],
+    activeNoteIndex: null
+}
+
+export function notes(state = initialState, action) {
+    switch (action.type) {
+        case actions.ACTIVATE_NOTE:
+            return {...state, activeNoteIndex: action.payload.noteIndex}
+
+        case actions.EDIT_COMPLETE:
+            const notes = [...state.notes]
+
+            notes[state.activeNoteIndex] = action.payload.note
+            
+            return {...state, notes: notes}
+        
+        default:
+            return state
+    }
+  }
