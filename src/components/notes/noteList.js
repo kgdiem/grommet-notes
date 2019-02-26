3744ddf2ca4fbcc15eb4816f0b01f22cea0e412c
Kevin <kevin@Kevins-MacBook-Pro.local>
2019-02-26 17:03:51 -0500

    right align erase button in note list

--- src/components/notes/noteList.js
+++ src/components/notes/noteList.js
@@ -1,22 +1,35 @@
 import React from 'react';
-import { Anchor, Box, InfiniteScroll, Paragraph } from 'grommet';
+import { Anchor, Box, Grid, InfiniteScroll, Paragraph } from 'grommet';
 import { Erase } from 'grommet-icons';
 
 const getNotePreview = note => note.content && note.content.slice(0, 20) || 'New note'
 
 const NoteListItem = ({note, onClick, onClickDelete}) => (
     <Box
+        alignContent="start"
         flex={false}
         pad="medium"
     >
-        <Paragraph margin="none">
-            <Anchor onClick={onClick}>
-                {getNotePreview(note)}
-            </Anchor>
-            <Anchor onClick={onClickDelete}>
-                <Erase/>
-            </Anchor>
-        </Paragraph>
+        <Grid
+            rows={['xsmall']}
+            columns={['small', 'xxsmall']}
+            areas={[
+                { name: 'preview', start: [0, 0], end: [0, 0] },
+                { name: 'actions', start: [1,0], end: [1, 0]}
+            ]}
+        >
+            <Box gridArea="preview">
+                <Anchor onClick={onClick}>
+                    {getNotePreview(note)}
+                </Anchor>
+            </Box>
+
+            <Box gridArea="actions">
+                <Anchor onClick={onClickDelete}>
+                    <Erase/>
+                </Anchor>
+            </Box>
+        </Grid>
     </Box>
 )
 
