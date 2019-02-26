a807a7a9fc24e9a0cf925bc92cb3ba173f264606
Kevin Diem <kgd003@apca938.833chestnut.tju.edu>
2019-02-25 22:16:25 -0500

    add grommet to app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import { Grommet } from 'grommet';
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <Grommet>
+        <div>
 
-      </div>
+        </div>
+      </Grommet>
     );
   }
 }
