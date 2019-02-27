0aca797c3b83c9a377d0df77dd9779318a950d9c
Kevin Diem <kgd003@apca938.833chestnut.tju.edu>
2019-02-27 14:45:53 -0500

    add useEffect hook to focus textarea on change

--- src/components/notes/noteArea.js
+++ src/components/notes/noteArea.js
@@ -1,26 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Box, TextArea, Collapsible } from 'grommet';
 
-export const NoteArea = ({note, onChange, visible}) => (
-    <Box fill flex background="light-1">
-        <Collapsible open={visible}>
-            <TextArea 
-                fill
-                flex
-                value={note && note.content ? note.content : ''} 
-                onChange={e => onChange(e.target.value)}
-                style={{
-                    height: '100vh',
-                    borderRadius: 0,
-                    border: 'none',
-                    overflow: 'auto',
-                    outline: 'none',
-                    WebkitBoxShadow: 'none',
-                    MozBoxShadow: 'none',
-                    boxShadow: 'none',
-                    resize: 'none',
-                }}
-            />
-        </Collapsible>
-    </Box>
-)
+export const NoteArea = ({note, onChange, visible}) => {
+    
+    useEffect(() => note && document.getElementById('noteTextArea').focus())
+
+    return (
+        <Box fill flex background="light-1">
+            <Collapsible open={visible}>
+                <TextArea 
+                    fill
+                    flex
+                    id="noteTextArea"
+                    value={note && note.content ? note.content : ''} 
+                    onChange={e => onChange(e.target.value)}
+                    style={{
+                        height: '100vh',
+                        borderRadius: 0,
+                        border: 'none',
+                        overflow: 'auto',
+                        outline: 'none',
+                        WebkitBoxShadow: 'none',
+                        MozBoxShadow: 'none',
+                        boxShadow: 'none',
+                        resize: 'none',
+                    }}
+                />
+            </Collapsible>
+        </Box>
+    )
+}
